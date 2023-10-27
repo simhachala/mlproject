7e1246b23075cc544487931f1c1919adada2f2de
simhachala <simha.avi005@gmail.com>
2023-10-27 04:00:28 -0400

    EDA and probem Statement

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import find_packages, 
+from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
-    this function will retyunr the list of requirements
+    this function will return the list of requirements
     '''
     requirements = []
     with open(file_path) as file_obj:
